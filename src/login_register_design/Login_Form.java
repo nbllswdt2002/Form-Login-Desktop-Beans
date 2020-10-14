a451b281661c7b0510a5c3b57911df3a63cf9700
stroberi_swdt <nabilla_swadati@yahoo.com>
2020-10-14 17:21:04 +0700

    insert username and password

--- src/login_register_design/Login_Form.java
+++ src/login_register_design/Login_Form.java
@@ -24,10 +24,15 @@ public class Login_Form extends javax.swing.JFrame {
         //center the form
         this.setLocationRelativeTo(null);
         
-        //create a yellow border for the jpanel_title
+        //create a orange border for the jpanel_title
         Border jpanel_title_border = BorderFactory.createMatteBorder(0, 1, 1, 1, Color.orange);
         //set the border to the jPanel_title
         jPanel_title.setBorder(jpanel_title_border);
+        
+        //create a black border for the close & minimize jlables
+        Border label_min_close_border = BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK);
+        jLabel_min.setBorder(label_min_close_border);
+        jLabel_close.setBorder(label_min_close_border);
     }
 
     /**
@@ -41,8 +46,9 @@ public class Login_Form extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jPanel2 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
+        label_username = new javax.swing.JLabel();
+        jLabel_min = new javax.swing.JLabel();
+        jLabel_close = new javax.swing.JLabel();
         jPanel_title = new javax.swing.JPanel();
         jLabel3 = new javax.swing.JLabel();
 
@@ -56,18 +62,26 @@ public class Login_Form extends javax.swing.JFrame {
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(21, 21, 21)
+                .addComponent(label_username, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 382, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(45, 45, 45)
+                .addComponent(label_username, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(299, Short.MAX_VALUE))
         );
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        jLabel1.setText("-");
+        label_username.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/username_field.png")));
 
-        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        jLabel2.setText("X");
+        jLabel_min.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel_min.setText("  -");
+
+        jLabel_close.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel_close.setText("  X");
 
         jPanel_title.setBackground(new java.awt.Color(255, 153, 204));
         jPanel_title.setFont(new java.awt.Font("Tahoma", 1, 48)); // NOI18N
@@ -99,13 +113,13 @@ public class Login_Form extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(137, Short.MAX_VALUE)
+                .addContainerGap(110, Short.MAX_VALUE)
                 .addComponent(jPanel_title, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(150, 150, 150)
-                .addComponent(jLabel1)
-                .addGap(18, 18, 18)
-                .addComponent(jLabel2)
-                .addGap(19, 19, 19))
+                .addGap(161, 161, 161)
+                .addComponent(jLabel_min, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel_close, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -114,8 +128,8 @@ public class Login_Form extends javax.swing.JFrame {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addContainerGap()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel1)
-                            .addComponent(jLabel2)))
+                            .addComponent(jLabel_min)
+                            .addComponent(jLabel_close)))
                     .addComponent(jPanel_title, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 26, Short.MAX_VALUE)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -171,11 +185,12 @@ public class Login_Form extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel_close;
+    private javax.swing.JLabel jLabel_min;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel_title;
+    private javax.swing.JLabel label_username;
     // End of variables declaration//GEN-END:variables
 }
